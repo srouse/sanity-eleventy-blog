60be9e96dcf8b7d5f2ffccee2bcd1cd141b8a696
Scott Rouse <scott.rouse@gmail.com>
2022-05-09 09:37:27 -0500

    moving to web-simple deploy

--- web-simple/src/routes.mjs
+++ web-simple/src/routes.mjs
@@ -5,23 +5,26 @@ import postData from './_data/post.mjs';
 import posts from './_data/posts.mjs';
 
 export default async function getRoutes(data) {
-  // global to each page...
+  // ====== global to each page =====
   // both browser and server
   await metadata(data);
+  await postsList(data); // can't make a post lookup w/o
+
   // server only...
   if (data.context.type === 'server') {
-    await posts(data);// postData will look for these first on server
+    await posts(data);// 'postData' will look for this data in the server build
   }
 
+  // Routes
   const routes = {
     routes: {
-      '/': {
+      [routeLookup('home')]: {
         template: 'index.njk',
         data: async (data) => {
           data.body = 'index page';
         }
       },
-      '/blog': {
+      [routeLookup('blogIndex')]: {
         template: 'blog/blogIndex.njk',
         data: async (data) => {
           await posts(data);
@@ -31,8 +34,7 @@ export default async function getRoutes(data) {
     }
   }
 
-  // dynamically add all the blog posts to the list
-  await postsList(data);
+  // Dynamically Routes
   data.postsList.map( post => {
     const url = routeLookup(post._type, post.slug);
     routes.routes[url] = {
